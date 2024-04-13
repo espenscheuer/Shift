3d54e141a670e6b86bab6822cbfd7b29c1df2772
espenscheuer <35115819+espenscheuer@users.noreply.github.com>
2024-04-13 09:24:10 -0700

    Initial Commit

---
+++ Shift/ShiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShiftApp.swift
+//  Shift
+//
+//  Created by Espen Scheuer on 4/13/24.
+//
+
+import SwiftUI
+
+@main
+struct ShiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
